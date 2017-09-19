c7a80975e2d1dd9dabff5d87fb0a8bf850a776d9
hiteshg <hg0111@gmail.com>
2017-09-20 03:36:19 +0530

    modified utils

--- play_around.py
+++ play_around.py
@@ -1,19 +1,8 @@
-from hiteshutils import basicutils as bu
-import requests
-import urllib
-from PIL import Image
+from datetime import datetime as dt
+from hiteshutils import basicutils
+import pandas_datareader.data as web
+
+for date in basicutils.daterange(start_date=dt(2017,1,20),end_date=dt.today()):
 
 
-def file_download(file_path, file_url):
-    r = requests.get(file_url, stream=True)
-    with open(file_path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=256):
-            fd.write(chunk)
-
-
-
-
-print("Start")
-exist = file_download(file_path='/Users/hiteshg/sample_file.zip',
-                 file_url='https://www.nseindia.com/archives/fo/mkt/fo11092000.zip')
-print(exist)
+    pass
